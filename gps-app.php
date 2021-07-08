d26dc4372609ad7c28901884731cccb82aaa757c
Your Name <72456481+ssmithgannett@users.noreply.github.com>
2021-07-08 11:21:50 -0500

    refinement for content, responsiveness, footer placehold

--- gps-app.php
+++ gps-app.php
@@ -1,7 +1,7 @@
 
 <?php
 /**
- * Plugin Name: GPS Application System
+ * Plugin Name: GPS Application System (this one)
  * Plugin URI:
  * Description: Updated application and recruitment system for paper delivery.
  * Version: 1.0
@@ -9,38 +9,74 @@
  * Author URI: http://despace.design
  */
 
+add_shortcode('gps-app-header', 'gps_app_header');
 
-//MARKET SUMMARY
+function gps_app_header() {
+  $featured_img_url = get_the_post_thumbnail_url(get_the_ID(),'full');
 
-add_shortcode('gps-form', 'gps_app');
-
-function gps_app() {
+  $custom_logo_id = get_theme_mod( 'custom_logo' );
+  $image = wp_get_attachment_image_src( $custom_logo_id , 'full' );
+  $logo = $image[0];    
 
   return '
     
-    <div id="app-container">
-      <header id="delivery-masthead">
-        <div id="site-brand">Gannett Carriers</div>
-        <ul>
-          <li><a href="#">What we offer</a></li>
-          <li><a href="#">How it works</a></li>
-          <li><a href="#">FAQs</a></li>
-        </ul>
-      </header>
-      <h1 id="apply-now">Apply to be a carrier</h1>
-      <div id="zip-module">
-        <p>Enter your zip code to begin your application.</p>
-        <div>
-          <input type="text" name="zip" id="enter-zip">
-          <button id="start-app">Continue</button>
-        </div>  
-      </div>
-      
+  <header id="delivery-masthead">
+    <img id="site-brand" src="' .  $logo .'" />
+    <div id="burger">
+      <div class="burger-line"></div>
+      <div class="burger-line"></div>
+      <div class="burger-line"></div>
     </div>
+    <ul id="gps-menu">
+    <div class="close">&times;</div>
+    <li><a href="#how-it-works">How it works</a></li>
+      <li><a href="#what-we-offer">What we offer</a></li>
+      <li><a href="#faq">FAQs</a></li>
+    </ul>
+  </header>
 
-    <div id="apply-cta"><a href="#apply-now">Apply Now</a></div>
+  <div class="hero-image" style="background-image:linear-gradient(rgba(0, 0, 0, 0.7),rgba(255,255, 255, 1)),url(' . $featured_img_url . ');">
+    <div class="hero-text"></div>
+  </div>
 
-    <script type="text/javascript" src="' . plugin_dir_url( __FILE__ ) . 'js/gps-app.js"></script>
+  <div id="apply-cta"><a href="#zip-module">Apply Now</a></div>
+  <script type="text/javascript" src="' . plugin_dir_url( __FILE__ ) . 'js/gps-app.js"></script>
+  ';
+}
+
+add_shortcode('place-form', 'form_module');
+
+function form_module() {
+  return '
+  <div id="form-module">
+    <h2>Apply Now</h2>
+    <div id="zip-module">
+      <p>Enter your zip code to begin your application.</p>
+      <div>
+        <input type="text" name="zip" id="enter-zip">
+        <button id="start-app">Continue</button>
+      </div>
+    </div>
+    
+
+    <div id="form-here"></div>
+  </div>
+
+  <footer id="gps-footer">?</footer>
+  
+  ';
+}
+
+add_shortcode('available-routes', 'available_routes');
+
+function available_routes() {
+  return '
+    <div id="available-routes">
+      <h3>Available Routes</h3>
+      <div id="returned-routes"></div>
+    </div>
+    <script type="text/javascript" src="' . plugin_dir_url( __FILE__ ) . 'js/routes.js"></script>
+
   ';
 }
 
--- js/gps-app.js
+++ js/gps-app.js
@@ -1,9 +1,19 @@
 jQuery(document).ready(function() {
   jQuery('.site-footer').remove()
   jQuery('#masthead').remove()
+  jQuery('.post-thumbnail').remove()
   jQuery('.site-content').css('margin-top', 0)
   jQuery('.entry-content').css('margin-top', 0)
-})
+  const headlineText = jQuery('.entry-content h1:nth-child(1)').text();
+  const subheadText = jQuery('.entry-content h3:nth-child(2)').text();
+  jQuery('.entry-content h3:nth-child(2)').remove()
+  jQuery('.entry-content h1:first-child').remove()
+
+  jQuery('.hero-text').append(`
+  <h1>${headlineText}</h1>
+  <p>${subheadText}<p>
+  `)
+
 
 jQuery('#start-app').on('click', function(e) {
   const zip = jQuery('#enter-zip').val();
@@ -20,7 +30,7 @@ success : function(data) {
       }
   
       const url = `https://gannett-nxuao.formstack.com/forms/prepop_test?zipcode=${zip}&region=${region}&property=${property}`;
-      jQuery('#app-container').append(`<iframe width="100%" height="2050" style="border:none;" src="${url}"></iframe>`);
+      jQuery('#form-here').append(`<iframe width="100%" height="2050" style="border:none;" src="${url}"></iframe>`);
       jQuery('#zip-module').fadeOut();
 
 },
@@ -29,4 +39,16 @@ console.log("Request: "+JSON.stringify(request));
 }
               
 });// end ajax       
-})
+});// start app
+
+jQuery('#burger').on('click', function() {
+  jQuery('#gps-menu').addClass('show')
+});
+
+jQuery('#gps-menu .close').on('click', function() {
+  jQuery('#gps-menu').removeClass('show')
+});
+
+
+});//page load
+
